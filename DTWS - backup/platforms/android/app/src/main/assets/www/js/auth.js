9f85b3a44557f21cef05a8a4d0875649a14998e6
Thomaach <thomas.wyns@student.odisee.be>
2019-06-12 16:25:16 +0200

    backup maken voor plugins

---
+++ DTWS - backup/platforms/android/app/src/main/assets/www/js/auth.js
@@ -0,0 +1,42 @@
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // get user info
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+
+  // log the user in
+  auth.signInWithEmailAndPassword(email, password).then((cred) => {
+    console.log("ik zit in sign in")
+    window.location.href = '../pages/MainMenu.html'
+  });
+});
+
+//signup
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // get user info
+  const email = signupForm['signup-email'].value;
+  const password = signupForm['signup-password'].value;
+
+  // sign up the user
+  auth.createUserWithEmailAndPassword(email, password).then(cred => {
+    window.location.href = 'pages/MainMenu.html'
+  })
+});
+
+
+//varibale user maken
+var user;
+
+//checken of use is ingelogd
+auth.onAuthStateChanged(function (currentUser) {
+  if (currentUser) {
+    console.log("logged in");
+    user = currentUser;
+    window.location.href = 'pages/MainMenu.html';
+  }
+})
---
+++ DTWS - backup/www/js/authMenu.js
@@ -0,0 +1,52 @@
+//varibale user maken
+var user;
+
+//checken of use is ingelogd
+auth.onAuthStateChanged(function (currentUser) {
+  if (currentUser) {
+    console.log("logged in");
+    user = currentUser;
+    addUser();
+  } else {
+    window.location.href = '../index.html'
+    console.log("not logged in");
+  }
+});
+
+function addUser() {
+  var UserRef = db.collection("Users");
+  var query = UserRef.where("UserID", "==", user.uid)
+  var count = 0;
+  query.get().then(function (querySnapshot) {
+    querySnapshot.forEach(function () {
+      count++;
+      console.log(count);
+    });
+    if (count == 0) {
+      db.collection('Users').add({
+        UserID: user.uid,
+        Email: user.email
+      });
+      console.log("user added");
+    }
+  });
+}
+
+
+//user info weergeven
+const profile = document.querySelector('#profile');
+profile.addEventListener('click', (e) => {
+  e.preventDefault();
+  //M.toast();
+  alert("id:" + user.uid + "\n" + "email: " + user.email);
+  console.log(user.uid)
+})
+
+
+//user uitloggen
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+  e.preventDefault();
+  auth.signOut();
+  window.location.href = '../index.html'
+});
